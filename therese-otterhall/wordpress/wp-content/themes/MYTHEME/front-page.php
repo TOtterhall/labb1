2e634a6671420ebfcefe80503adfa7d06e865b55
Therese Otterhäll <tessan.otterhall@gmail.com>
2023-04-17 23:16:10 +0200

    add changes

--- therese-otterhall/wordpress/wp-content/themes/MYTHEME/front-page.php
+++ therese-otterhall/wordpress/wp-content/themes/MYTHEME/front-page.php
@@ -2,15 +2,15 @@
 get_header();
 ?>
 
-
+<main>
 			<section>
 				<div class="container">
 					<div class="row">
 						<div class="col-xs-12">
 							<div class="hero">
-								<?php the_post_thumbnail("full");?>
+								<?php the_post_thumbnail('full');?>
 								<div class="text">
-									<h1><?php the_title();?></h1>
+									<h1></h1>
 									<p><?php the_content();?></p>
 								</div>
 							</div>
@@ -19,7 +19,7 @@ get_header();
 				</div>
 			</section>
 		
-
+</main>
 
 
 
